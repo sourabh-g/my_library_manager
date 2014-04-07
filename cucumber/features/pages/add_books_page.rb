83e5c55300a52d3721e4f9d4ac9e0fc7d6fb7b04
sourabh-g <sourabhg@thoughtworks.com>
2014-04-07 19:19:59 +0530

    added detail filling within form

--- cucumber/features/pages/add_books_page.rb
+++ cucumber/features/pages/add_books_page.rb
@@ -7,11 +7,11 @@ module Pages
     end
 
     def create_book
-      #within('#field') do
-      fill_in 'book[title]', with: 'Dummy Title'
-      fill_in 'book[author]', with: 'Dummy Author'
+      within('#field') do
+        fill_in 'book[title]', with: 'Dummy Title'
+        fill_in 'book[author]', with: 'Dummy Author'
+      end
       click_on 'Create Book'
-      #end
     end
   end
 end
